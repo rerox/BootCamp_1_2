a41fcfbb66946bfe74a1c8953db3436e66830dfa
rerox <mateusz.nosek93@gmail.com>
2017-04-07 21:37:06 +0200

    Second exercise

---
+++ js.js
@@ -0,0 +1,11 @@
+// Wyszukiwanie selektorĂłw //
+
+$("div.grid.grid12");
+$("a[href^='http']");
+$(".nav>a[href^='http']");
+$("input:radio:not(:checked), input:checkbox:not(:checked)");
+$("div#text p:empty:first");
+$(".pagination-item:not(span)");
+
+
+// Koniec wyszukiwania selektorĂłw //
